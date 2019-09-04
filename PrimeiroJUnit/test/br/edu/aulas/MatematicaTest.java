55fce5a9d34233e447dfa6c8091ab34ac434d4c3
Thiago Goulart <thiaggoulart@gmail.com>
2019-09-04 09:13:37 -0300

    termina teste

--- PrimeiroJUnit/test/br/edu/aulas/MatematicaTest.java
+++ PrimeiroJUnit/test/br/edu/aulas/MatematicaTest.java
@@ -28,71 +28,152 @@ public class MatematicaTest {
 	public void testaSomaPositivo() {
 		assertEquals("2 + 2 deve ser 4", 4, matematica.soma(2, 2));
 	}
-	
+
 	@Test
 	public void testaSomaPositivoComNegativo() {
 		assertEquals("2 + (-2) deve ser 0", 0, matematica.soma(2, -2));
 	}
-	
+
 	@Test
 	public void testaSomaNegativo() {
 		assertEquals("-2 + (-2) deve ser -4", -4, matematica.soma(-2, -2));
 	}
-	
+
 	@Test
 	public void testaSomaPositivoComZero() {
 		assertEquals("2 + 0 deve ser 2", 2, matematica.soma(2, 0));
 	}
-	
+
 	@Test
 	public void testaSomaNegativoComZero() {
 		assertEquals("-2 + 0 deve ser -2", -2, matematica.soma(-2, 0));
 	}
-	
+
 	@Test
 	public void testaSomaZeroComZero() {
 		assertEquals("0 + 0 deve ser 0", 0, matematica.soma(0, 0));
 	}
-	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testaOLancamentoDaExcecaoDaSoma() {
+		matematica.soma(1000, 2);
+	}
+
 	@Test
 	public void testaSubtraiPositivo() {
 		assertEquals("2 - 2 deve ser 0", 0, matematica.subtrai(2, 2));
 	}
-	
+
 	@Test
 	public void testaSubtraiPositivoComNegativo() {
 		assertEquals("2 - (-2) deve ser 4", 4, matematica.subtrai(2, -2));
 	}
-	
+
 	@Test
 	public void testaSubtraiNegativo() {
 		assertEquals("(-2) - (-2) deve ser 0", 0, matematica.subtrai(-2, -2));
 	}
-	
+
 	@Test
 	public void testaSubtraiPositivoComZero() {
 		assertEquals("2 - 0 deve ser 2", 2, matematica.subtrai(2, 0));
 	}
-	
+
 	@Test
 	public void testaSubtraiNegativoComZero() {
 		assertEquals("-2 - 0 deve ser -2", -2, matematica.subtrai(-2, 0));
 	}
-	
+
 	@Test
 	public void testaSubtraiZeroComZero() {
 		assertEquals("0 - 0 deve ser 0", 0, matematica.subtrai(0, 0));
 	}
-	
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testaOLancamentoDaExcecao() {
-		matematica.multiplica(1000, 5);
+	public void testaOLancamentoDaExcecaoDaSubtracao() {
+		matematica.subtrai(1000, 2);
 	}
 
 	@Test
-	public void testMultiplica() {
-		assertEquals("10 x 5 deve ser 50", 50, matematica.multiplica(10, 5));
+	public void testaMutiplicaPositivo() {
+		assertEquals("2 x 2 deve ser 4", 4, matematica.multiplica(2, 2));
+
+	}
+
+	@Test
+	public void testaMutiplicaPositivoComNegativo() {
+		assertEquals("2 x -2 deve ser -4", -4, matematica.multiplica(2, -2));
+
+	}
+
+	@Test
+	public void testaMutiplicaNegativo() {
+		assertEquals("-2 x -2 deve ser 4", 4, matematica.multiplica(-2, -2));
+
+	}
+
+	@Test
+	public void testaMutiplicaPositivoComZero() {
+		assertEquals("2 x 0 deve ser 0", 0, matematica.multiplica(2, 0));
+
+	}
+
+	@Test
+	public void testaMutiplicaNegativoComZero() {
+		assertEquals("-2 x 0 deve ser 0", 0, matematica.multiplica(-2, 0));
+
 	}
 	
+	@Test
+	public void testaMutiplicaZeroComZero() {
+		assertEquals("0 x 0 deve ser 0", 0, matematica.multiplica(0, 0));
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testaOLancamentoDaExcecaoDaMultiplicacao() {
+		matematica.multiplica(1000, 2);
+	}
+	
+	@Test
+	public void testaDividePositivo() {
+		assertEquals("2 / 2 deve ser 1", 1, matematica.divide(2, 2));
+
+	}
+
+	@Test
+	public void testaDividePositivoComNegativo() {
+		assertEquals("2 / -2 deve ser -1", -1, matematica.divide(2, -2));
+
+	}
+
+	@Test
+	public void testaDivideNegativo() {
+		assertEquals("-2 x -2 deve ser 1", 1, matematica.divide(-2, -2));
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testaDividePositivoComZero() {
+		matematica.divide(2, 0);
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testaDivideNegativoComZero() {
+		assertEquals("-2 x 0 deve ser 0", 0, matematica.divide(-2, 0));
+
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testaDivideZeroComZero() {
+		assertEquals("0 x 0 deve ser 0", 0, matematica.divide(0, 0));
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testaOLancamentoDaExcecaoDaDivisao() {
+		matematica.divide(1000, 2);
+	}
+
 }
